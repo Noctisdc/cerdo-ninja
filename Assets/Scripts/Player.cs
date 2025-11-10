c0be53d6923013e2469cad260e9e033e5fca8335
Noctisdc <camijavier@hotmail.com>
2025-11-10 02:22:07 -0500

    Modificacion del mapa, animaciones, tilemaps, movimiento y scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 4f;
+
+    private Rigidbody2D rb2D;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Animator animator;
+
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
+
+        if (move != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
+        }
+
+             animator.SetFloat("Speed", Mathf.Abs(move));
+             animator.SetFloat("VerticalVelocity", rb2D.linearVelocity.y);
+             animator.SetBool("isGrounded", isGrounded);
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+    }
+}
